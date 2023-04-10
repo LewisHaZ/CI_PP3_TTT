123040f41d62512075d9a6ad80e35f9d2e069c3c
LewisHaZ <feldman.lewis@Gmail.com>
2023-04-10 08:22:22 +0000

    add scope and vars to validation for the sheets input (feature)

--- validation.py
+++ validation.py
@@ -3,4 +3,12 @@ import gspread
 from google.oauth2.service_account import Credentials
 from email_validator import validate_email, EmailNotValidError
 from colors import Color as Col
-from run import run_game, cls, separate_line
+from run import start_game, cls, separate_line
+
+# Scope and constant vars defined as in love_sandwiches walk-through project
+# by Code Institute
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
