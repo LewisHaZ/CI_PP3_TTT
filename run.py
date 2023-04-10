aadbeadd34ecd74aac1da9b174a84820cb521371
LewisHaZ <feldman.lewis@Gmail.com>
2023-04-10 07:00:29 +0000

    adjust print message for not valid while loop (fix)

--- run.py
+++ run.py
@@ -65,7 +65,7 @@ def handle_turn(player):
     while not valid:
 
         while position not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
-            position = input("Invalid input. Choose a position from 1-9: ")
+            position = input("Choose a position from 1-9: ")
 
         position = int(position) - 1
 
