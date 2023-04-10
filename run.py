54c21e2210e0b1cf44d9bb740801fc461d7c1a0e
LewisHaZ <feldman.lewis@Gmail.com>
2023-04-10 05:21:21 +0000

    add game board and a function to display it (feature)

--- run.py
+++ run.py
@@ -5,4 +5,13 @@
 # Function to check for win or tie
 # Function to switch the turn from x to o
 
+board = ["-","-","-",
+         "-","-","-",
+         "-","-","-",]
 
+def display_board():
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+
+display_board()
