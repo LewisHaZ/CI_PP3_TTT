0a4327c5510db439a7c4264259fb6da2369e3d21
LewisHaZ <feldman.lewis@Gmail.com>
2023-04-14 23:36:14 +0000

    change colors file values (feature)

--- colors.py
+++ colors.py
@@ -4,8 +4,8 @@ init(autoreset=True)
 
 
 class Color:
-    BLUE = "\033[0;31;48m"
+    BLUE = "\033[0;34;48m"
     RED = "\033[0;31;48m"
-    YELLOW = "\033[0;31;48m"
-    LOGO_B = "\033[0;31;48m"
+    YELLOW = "\033[0;33;48m"
+    LOGO_B = "\033[0;34;48m"
     LOGO_R = "\033[0;31;48m"
